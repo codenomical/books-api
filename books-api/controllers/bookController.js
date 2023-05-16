7bb3a6b1515bda69edc95b199466cd56bc04ec91
Richard Quizon <codenomics@proton.me>
2023-05-15 18:32:50 -0700

    bookController now has post, put and delete

--- books-api/controllers/bookController.js
+++ books-api/controllers/bookController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const Book = require('../models/books');
 
 router.get('/', async (req, res) => {
     try {
@@ -22,6 +23,24 @@ router.get('/:id', async (req, res) => {
     catch (err) {
         res.status(500).json({ error: err.message });
     }
+});
+
+router.post('/', (req, res) => {
+    Book.create(req.body)
+        .then(newBook => res.status(201).json(newBook))
+        .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.put('/:id', (req, res) => {
+    Book.findByIdAndUpdate(req.params.id, req.body, { new: true})
+    .then(updateBook => res.status(200).json(updatedBook))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.delete('/:id', (req, res) => {
+    Book.findByIdAndDelete(req.params.id)
+    .then(() => res.status(200).json({ message: 'Book deleted successfully'}))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
 module.exports = router;
